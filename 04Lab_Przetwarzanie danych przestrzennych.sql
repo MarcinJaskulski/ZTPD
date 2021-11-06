a145a05dd5270f90416cb01d5ccff40b980848f2
Marcin Jaskulski <mrcn.jaskulski@gmail.com>
2021-11-06 21:46:04 +0100

    Add files via upload

---
+++ 04Lab_Przetwarzanie danych przestrzennych.sql
@@ -0,0 +1,62 @@
+--zad1A
+create table FIGURY(
+    ID NUMBER(1) PRIMARY KEY,
+    KSZTALT MDSYS.SDO_GEOMETRY
+)
+
+--zada1B
+insert into figury values(
+    1, 
+    MDSYS.SDO_GEOMETRY(
+        2003, -- w 2 wymiarach - wielokat
+        null, -- jaki ukad wspolrzednych - null - kartezjanski
+        null, -- 
+        SDO_ELEM_INFO_ARRAY(1,1003,4), -- pozycja pierwszej kookrynaty,  typ elementu, interpretacja
+        -- 1- pozycja, wielokat zewnwtrzny, kolo
+        SDO_ORDINATE_ARRAY(3,5 ,5,3 ,7,5) -- koordynaty poszczegolnych punktow
+    )
+);
+			
+insert into figury values(
+    2,
+    MDSYS.SDO_GEOMETRY(
+        2003, 
+        NULL, 
+        NULL, 
+        MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,3),
+        MDSYS.SDO_ORDINATE_ARRAY(1,1, 5,5)
+    )
+);	
+						
+insert into figury values(
+    3,
+    MDSYS.SDO_GEOMETRY(
+        2002, -- w 2 wymiarach - ciagla linia
+        null,
+        null,
+        SDO_ELEM_INFO_ARRAY(1,4,2, 1,2,1 ,5,2,2),
+        SDO_ORDINATE_ARRAY(3,2 ,6,2 ,7,3 ,8,2, 7,1)
+    )
+);
+
+--zad1C
+insert into figury values(
+    4,
+    MDSYS.SDO_GEOMETRY(
+        2003, 
+        NULL, 
+        NULL, 
+        MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,3),
+        MDSYS.SDO_ORDINATE_ARRAY(1,1, 1,1)
+    )
+);
+
+
+
+
+
+
+
+
+
+
