b7cee119aa405c30e1fc573e72f9d4849a3cd842
Marcin Jaskulski <mrcn.jaskulski@gmail.com>
2021-11-06 21:46:42 +0100

    Add files via upload

--- 04Lab_Przetwarzanie danych przestrzennych.sql
+++ 04Lab_Przetwarzanie danych przestrzennych.sql
@@ -40,17 +40,28 @@ insert into figury values(
 );
 
 --zad1C
+--ko³o zdefiniowane przez punkty le¿¹ce na prostej,
 insert into figury values(
-    4,
+    4, 
     MDSYS.SDO_GEOMETRY(
-        2003, 
-        NULL, 
-        NULL, 
-        MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,3),
-        MDSYS.SDO_ORDINATE_ARRAY(1,1, 1,1)
+        2003,
+        null,
+        null,
+        SDO_ELEM_INFO_ARRAY(1,1003,4),
+        SDO_ORDINATE_ARRAY(3,5 ,5,5 ,7,5)
     )
 );
 
+--zad1D
+SELECT id, SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(ksztalt,0.01)
+from figury;
+
+--zad1E	
+Delete from figury
+where SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(ksztalt,0.01) <> 'TRUE'
+
+--zad1F
+COMMIT;
 
 
 
